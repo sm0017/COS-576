7e9737c429355a1273bd5a3cc2603cc0a88bb959
sm0017 <smita.sukhadeve@maine.edu>
2015-07-19 18:10:55 -0400

    spring in practice recepies

---
+++ sip03/recipe-3.1/src/com/springinpractice/ch03/model/Member.java
@@ -0,0 +1,43 @@
+
+package com.springinpractice.ch03.model;
+/* 
+ * Copyright (c) 2013 Manning Publications Co.
+ * 
+ * Book: http://manning.com/wheeler/
+ * Blog: http://springinpractice.com/
+ * Code: https://github.com/springinpractice
+ */
+
+
+public class Member {
+	private String firstName;
+	private String lastName;
+	
+	public Member() {
+	}
+	
+	public Member(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	public String toString() {
+		return firstName + " " + lastName;
+	}
+}
