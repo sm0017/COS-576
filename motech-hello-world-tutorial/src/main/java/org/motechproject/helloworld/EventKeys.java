5c5aa143079b6d3ff2e784856510df573c4eea68
sm0017 <smita.sukhadeve@maine.edu>
2015-09-15 22:41:52 -0400

    Motech Helloworld

---
+++ motech-hello-world-tutorial/src/main/java/org/motechproject/helloworld/EventKeys.java
@@ -0,0 +1,9 @@
+package org.motechproject.helloworld;
+
+public final class EventKeys {
+
+    public static final String HELLO_SUBJECT = "Hello, World!";
+
+    private EventKeys() {
+    }
+}
---
+++ motech-hello-world-tutorial/src/main/java/org/motechproject/helloworld/web/EventEmitterController.java
@@ -0,0 +1,23 @@
+package org.motechproject.helloworld.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.motechproject.helloworld.event.EventEmitter;
+
+@Controller
+public class EventEmitterController {
+    
+    @Autowired
+    private EventEmitter eventEmitter;
+
+    @RequestMapping("/event-emitter")
+    @ResponseBody
+    public String emitEvent() {
+        eventEmitter.emitEvent();
+        String message = "Will emit an event with subject " + eventEmitter.getSubject();
+        return message;
+    }
+}
